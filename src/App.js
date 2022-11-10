ccfddb111e4be98a5a7d7dd653eedaf835de8eef
ChristinaDo <109245422+ChristinaDo@users.noreply.github.com>
2022-11-10 11:48:14 +0100

    imported weather app from CodeSandbox

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 import React from "react";
 import Weather from "./Weather";
@@ -6,18 +5,7 @@ import Weather from "./Weather";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Weather />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Click here
-        </a>
-      </header>
+      <Weather />
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,59 @@
 import React from "react";
-import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Forecast from "./Forecast";
 
-export default function ShowForecast() {
-  return <h1>Hello World</h1>;
+export default function searchWeather() {
+  return (
+    <div className="App">
+      <div className="weatherApp">
+        <form>
+          <input
+            className="city"
+            type="text"
+            placeholder="Enter a city..."
+            autocomplete="off"
+          />
+          <input className="search" type="submit" value="Search" />
+        </form>
+        <br />
+
+        <div className="row">
+          <div className="col-4">
+            <h1>Vienna</h1>
+            <p className="date description">
+              Thursday, 13 October 2022 partly cloudy
+            </p>
+          </div>
+          <div className="col-4">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+              alt="cloud"
+              className="cloud"
+            />
+          </div>
+          <div className="col-4">
+            <h2>19°C</h2>
+
+            <p>
+              Humidity: 79%
+              <br />
+              Wind: 8 km/h
+            </p>
+          </div>
+        </div>
+        <Forecast />
+      </div>
+
+      <p className="reference">
+        <a
+          href="https://github.com/ChristinaDo/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open-source code
+        </a>{" "}
+        by Christina Doneus
+      </p>
+    </div>
+  );
 }
