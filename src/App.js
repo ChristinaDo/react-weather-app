6723987ee158c6aa64f345f200db83e27324414b
ChristinaDo <109245422+ChristinaDo@users.noreply.github.com>
2022-11-15 22:35:31 +0100

    added structure and CSS

--- src/App.js
+++ src/App.js
@@ -6,6 +6,18 @@ function App() {
   return (
     <div className="App">
       <Weather />
+      <footer>
+        <p className="reference">
+          <a
+            href="https://github.com/ChristinaDo/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-source code
+          </a>{" "}
+          by Christina Doneus
+        </p>
+      </footer>
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -1,59 +1,29 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./Weather.css";
 import Forecast from "./Forecast";
 
-export default function searchWeather() {
+export default function Weather() {
   return (
-    <div className="App">
-      <div className="weatherApp">
-        <form>
-          <input
-            className="city"
-            type="text"
-            placeholder="Enter a city..."
-            autocomplete="off"
-          />
-          <input className="search" type="submit" value="Search" />
-        </form>
-        <br />
-
-        <div className="row">
-          <div className="col-4">
-            <h1>Vienna</h1>
-            <p className="date description">
-              Thursday, 10 November 2022 partly cloudy
-            </p>
-          </div>
-          <div className="col-4">
-            <img
-              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-              alt="cloud"
-              className="cloud"
-            />
-          </div>
-          <div className="col-4">
-            <h2>19°C</h2>
-
-            <p>
-              Humidity: 79%
-              <br />
-              Wind: 8 km/h
-            </p>
-          </div>
-        </div>
-        <Forecast />
-      </div>
-
-      <p className="reference">
-        <a
-          href="https://github.com/ChristinaDo/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Open-source code
-        </a>{" "}
-        by Christina Doneus
-      </p>
+    <div className="Weather">
+      <form>
+        <input
+          className="searchField"
+          type="text"
+          placeholder="Enter a city..."
+          autocomplete="off"
+        />
+        <input className="searchButton" type="submit" value="Search" />
+      </form>
+      <h1>London</h1>
+      <p>Wednesday, 20:25</p>
+      <h2>6°C</h2>
+      <ul>
+        <li>sunny</li>
+        <li>20%</li>
+        <li>4 km/h</li>
+      </ul>
+      <Forecast />
     </div>
   );
 }
