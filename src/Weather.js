973974182b497d121d1be8d7f20640214015a408
ChristinaDo <109245422+ChristinaDo@users.noreply.github.com>
2022-11-15 23:48:12 +0100

    added API

--- src/Weather.js
+++ src/Weather.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./Weather.css";
+import axios from "axios";
 import Forecast from "./Forecast";
 
 export default function Weather() {
+  const apiKey = "ae243fbc2119a2o30cd00e9et7ff3edf";
+  let city = "London";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   return (
     <div className="Weather">
       <form>
