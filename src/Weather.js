a89fb433173a4104bdae18d2d86501ab0f7e5c50
ChristinaDo <109245422+ChristinaDo@users.noreply.github.com>
2022-11-16 15:11:06 +0100

    added API integration

--- src/Weather.js
+++ src/Weather.js
@@ -4,30 +4,48 @@ import "./Weather.css";
 import axios from "axios";
 import Forecast from "./Forecast";
 
-export default function Weather() {
-  const apiKey = "ae243fbc2119a2o30cd00e9et7ff3edf";
-  let city = "London";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-  return (
-    <div className="Weather">
-      <form>
-        <input
-          className="searchField"
-          type="text"
-          placeholder="Enter a city..."
-          autocomplete="off"
-        />
-        <input className="searchButton" type="submit" value="Search" />
-      </form>
-      <h1>London</h1>
-      <p>Wednesday, 20:25</p>
-      <h2>6°C</h2>
-      <ul>
-        <li>sunny</li>
-        <li>20%</li>
-        <li>4 km/h</li>
-      </ul>
-      <Forecast />
-    </div>
-  );
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  function handleResponse(response) {
+    setWeatherData({
+      ready: true,
+      city: response.data.city,
+      date: "Wednesday, 20:25",
+      temperature: response.data.temperature.current,
+      description: response.data.condition.description,
+      humidity: response.data.temperature.humidity,
+      wind: response.data.wind.speed,
+    });
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <form>
+          <input
+            className="searchField"
+            type="text"
+            placeholder="Enter a city..."
+            autocomplete="off"
+          />
+          <input className="searchButton" type="submit" value="Search" />
+        </form>
+        <h1>{weatherData.city}</h1>
+        <p>{weatherData.date}</p>
+        <h2>{Math.round(weatherData.temperature)}°C</h2>
+        <ul>
+          <li className="text-capitalize">{weatherData.description}</li>
+          <li>{weatherData.humidity}%</li>
+          <li>{Math.round(weatherData.wind)} km/h</li>
+        </ul>
+        <Forecast />
+      </div>
+    );
+  } else {
+    const apiKey = "ae243fbc2119a2o30cd00e9et7ff3edf";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${props.defaultCity}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+
+    return "Loading...";
+  }
 }
